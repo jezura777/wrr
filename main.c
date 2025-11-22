b0a560fd8652fb25d49c3c4f0d2978d7dffd244c
Jezura777 <luki.fi@seznam.cz>
2025-11-22 23:04:09 +0100

    added append mode and center mode

--- main.c
+++ main.c
@@ -1,29 +1,42 @@
 #include <stdio.h>
-#define NOB_STRIP_PREFIX
-#define NOB_IMPLEMENTATION
-#include "nob.h"
+
+#define CR "20"
+#define CC "30"
+
+int center=1;
+int append=0;
 
 #define BUFF_SIZE 512
 
 void cls()
 {
     printf("\e[1;1H\e[2J");
+    if (center) printf("\e[%s;%sH", CR, CC);
 }
 
 
 int main(int argc, char **argv)
 {
     if(argc < 2) { 
-        nob_log(ERROR,"output file expected but none provided");
+        printf("output file expected but none provided\n");
         return 1;
+    }
+
+    if(argc > 2 && argv[2][0]=='-' ){
+	if(argv[2][1]=='n') center=0;
+	if(argv[2][1]=='a') append=1;
     }
 
     char buff[BUFF_SIZE] = {0};
     char *line = NULL;
 
     cls();
-    FILE* out = fopen(argv[1], "w");
+    FILE* out = {0};
+    if(!append) out = fopen(argv[1], "w");
+    else out = fopen(argv[1], "a");
+
     for(;;) {
+	printf("\e[%sG", CC);
 	line = NULL;
         buff[0] = '\0';
         line = fgets(buff, BUFF_SIZE, stdin);
@@ -32,6 +45,7 @@ int main(int argc, char **argv)
         else fputs(buff, out);
     }
     cls();
+    printf("\e[H");
 
     printf("%s, written.\n", argv[1]);
 
